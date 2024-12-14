ca0234e6a91b432727122279d21a69cf8e02b448
lucasmiguelabreu <abreu.lucasrodrigues@gmail.com>
2024-12-14 16:22:44 -0300

    configuração do .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://kanban:QAFU3XsnKy9sPO3T@cluster0.vr2hs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
-    TasksModule
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], 
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'), 
+      }),
+      inject: [ConfigService], 
+    }),
+    TasksModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
